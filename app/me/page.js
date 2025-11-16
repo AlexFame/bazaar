b45240e3d4579eb91fee108a0d4576d2893a4f28
AlexFame <132550321+AlexFame@users.noreply.github.com>
2025-11-16 04:43:10 +0100

    fix build config

--- app/me/page.js
+++ app/me/page.js
@@ -1,9 +1,12 @@
-export default function Me(){
+"use client";
+export default function Me() {
   return (
     <main className="space-y-4">
       <div className="bg-white p-4 rounded-lg border">
         <div className="font-semibold">Мои объявления</div>
-        <div className="text-sm text-gray-500">В MVP список без редактирования. Позже добавим.</div>
+        <div className="text-sm text-gray-500">
+          В MVP список без редактирования. Позже добавим.
+        </div>
       </div>
     </main>
   );
