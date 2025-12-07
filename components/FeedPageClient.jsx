1c347f4c9f3ff9a96f5a0fa9d58df578e50d00e9
AlexFame <132550321+AlexFame@users.noreply.github.com>
2025-12-07 17:51:55 +0100

    Debug: Add version logging to bust cache

--- components/FeedPageClient.jsx
+++ components/FeedPageClient.jsx
@@ -262,6 +262,7 @@ export default function FeedPageClient({ forcedCategory = null }) {
 
   // Restore scroll on mount
   useEffect(() => {
+    console.log("FeedPageClient mounted. Version debug-940");
     if (listings.length > 0 && scrollPos > 0) {
         window.scrollTo(0, scrollPos);
     }
